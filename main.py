5d22c79ce74d65ee12132142468e0bc81468c7ff
Eskob12 <kubispavel84@seznam.cz>
2022-11-09 18:43:40 +0100

    update

--- main.py
+++ main.py
@@ -14,13 +14,43 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet):
+
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
-    columns = [[], [], []]
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        print()
 
 
 def deposit():
@@ -76,10 +106,9 @@ def main():
             print(f"You do not have enough balance, your current balance is: {balance}")
         else:
             break
-
-
     print(f"You are betting {bet} on {lines} lines. Total bet is equal to: {total_bet}")
-
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
