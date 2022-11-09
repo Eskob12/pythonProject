f02d5a19d2a9bf03151a845ada2cc0e2d683fda2
Eskob12 <kubispavel84@seznam.cz>
2022-11-09 18:17:07 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[], [], []]
 
 
 def deposit():
@@ -48,8 +68,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough balance, your current balance is: {balance}")
+        else:
+            break
+
+
     print(f"You are betting {bet} on {lines} lines. Total bet is equal to: {total_bet}")
 
 
